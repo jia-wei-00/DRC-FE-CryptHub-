dab61c39ac4e65d97dde79fe1f2e13401d9c16f0
jia-wei-00 <wwwbingo6@gmail.com>
2023-06-16 02:08:20 +0800

    update chart

--- src/pages/home/chart.tsx
+++ src/pages/home/chart.tsx
@@ -28,33 +28,13 @@ export const options = {
   responsive: true,
   plugins: {
     legend: {
-      position: "top" as const,
+      display: false,
     },
-    title: {
-      display: true,
-      text: "Chart.js Line Chart",
-    },
+    // title: {
+    //   display: true,
+    //   text: "Chart.js Line Chart",
+    // },
   },
-};
-
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Dataset 2",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
 };
 
 const Chart: React.FC = () => {
@@ -64,16 +44,22 @@ const Chart: React.FC = () => {
     labels,
     datasets: [
       {
-        label: "Dataset 1",
         data: apiStore.chart_data,
         borderColor: "rgb(255, 99, 132)",
         backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
+      // {
+      //   label: "ETH",
+      //   data: apiStore.chart_data_eth,
+      //   borderColor: "rgb(255, 99, 132)",
+      //   backgroundColor: "rgba(255, 99, 132, 0.5)",
+      // },
     ],
   };
 
   React.useEffect(() => {
     apiStore.subscribeTicks();
+    apiStore.subscribeTicksEth();
   }, []);
 
   //   console.log(apiStore.chart_data);
--- src/stores/api-store.tsx
+++ src/stores/api-store.tsx
@@ -7,10 +7,6 @@ const connection = new WebSocket(
   `wss://ws.binaryws.com/websockets/v3?app_id=${app_id}`
 );
 const api = new DerivAPIBasic({ connection });
-// const tickStream = () =>
-//   api.subscribe({
-//     ticks: "cryBTCUSD",
-//   });
 
 const tickHistory = () =>
   api.subscribe({
@@ -22,17 +18,33 @@ const tickHistory = () =>
     style: "ticks",
   });
 
+const tickHistoryETH = () =>
+  api.subscribe({
+    ticks_history: "cryETHUSD",
+    adjust_start_time: 1,
+    count: 19,
+    end: "latest",
+    start: 1,
+    style: "ticks",
+  });
+
 class ApiStoreImplementation {
   chart_data: number[] = [];
+  chart_data_eth: any[] = [];
   chart_labels: String[] = [];
+  chart_labels_eth: String[] = [];
 
   constructor() {
     makeObservable(this, {
       chart_data: observable,
       chart_labels: observable,
+      chart_data_eth: observable,
+      chart_labels_eth: observable,
       tickResponse: action.bound,
       subscribeTicks: action.bound,
       unsubscribeTicks: action.bound,
+      subscribeTicksEth: action.bound,
+      unsubscribeTicksEth: action.bound,
     });
 
     // // Make the store persistable
@@ -87,8 +99,52 @@ class ApiStoreImplementation {
     }
   };
 
+  tickResponseEth = async (res: any) => {
+    const data = JSON.parse(res.data);
+
+    if (data.error !== undefined) {
+      console.log("Error: ", data.error.message);
+      connection.removeEventListener("message", this.tickResponse, false);
+      await api.disconnect();
+    }
+
+    // if (data.msg_type === "history") {
+    //   runInAction(() => {
+    //     const formattedTimes = data.history.times.map(
+    //       (timestamp: EpochTimeStamp) => {
+    //         const date = new Date(timestamp * 1000); // Convert epoch to milliseconds
+    //         const hours = date.getHours(); // Get minutes from timestamp
+    //         const minutes = date.getMinutes(); // Get minutes from timestamp
+    //         const seconds = date.getSeconds(); // Get seconds from timestamp
+    //         return `${hours}:${minutes.toFixed(2)}:${seconds.toFixed(2)}`;
+    //       }
+    //     );
+    //     this.chart_data_eth = data.history.prices;
+    //     this.chart_labels_eth = formattedTimes;
+    //   });
+    // }
+
+    if (data.msg_type === "tick") {
+      runInAction(() => {
+        // const updatedData = [...this.chart_data, data.tick.quote];
+        // const timestamp = new Date(data.tick.epoch * 1000); // Convert epoch to milliseconds
+        // const hours = timestamp.getHours();
+        // const minutes = timestamp.getMinutes(); // Get minutes from timestamp
+        // const seconds = timestamp.getSeconds(); // Get seconds from timestamp
+        // const formattedTime = `${hours}:${minutes}:${seconds}`; // Format minutes and seconds
+        // const updatedLabels = [...this.chart_labels, formattedTime];
+        // if (updatedData.length > 20) {
+        //   updatedData.shift();
+        //   updatedLabels.shift();
+        // }
+        this.chart_data_eth = data.tick.quote;
+        this.chart_labels_eth = data.tick.epoch;
+      });
+    }
+  };
+
   subscribeTicks = async () => {
-    await tickHistory();
+    // await tickHistory();
     // await tickStream();
     connection.addEventListener("message", this.tickResponse);
   };
@@ -97,6 +153,18 @@ class ApiStoreImplementation {
     connection.removeEventListener("message", this.tickResponse, false);
     // tickStream().unsubscribe();
     tickHistory().unsubscribe();
+  };
+
+  subscribeTicksEth = async () => {
+    await tickHistoryETH();
+    // await tickStream();
+    connection.addEventListener("message", this.tickResponseEth);
+  };
+
+  unsubscribeTicksEth = () => {
+    connection.removeEventListener("message", this.tickResponseEth, false);
+    // tickStream().unsubscribe();
+    tickHistoryETH().unsubscribe();
   };
 }
 
