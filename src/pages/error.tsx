73074919ce50b1e01fd47064457de7afb9f9bd97
jia-wei-00 <wwwbingo6@gmail.com>
2023-07-14 19:39:08 +0800

    solve 404 page responsive and style

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,24 +1,24 @@
 import { Box, Button, Container, Typography } from "@mui/material";
-import Grid from "@mui/material/Grid";
-import Logo from "../assets/logo.svg";
+import Logo from "../assets/tab-icon.svg";
 import "../styles/pages/error.scss";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   return (
     <Box className="container">
       <Container maxWidth="md">
-        <Grid container spacing={2}>
-          <Grid xs={12} md={6}>
-            <Typography variant="h1">404</Typography>
-            <Typography variant="h6">
-              The page you’re looking for doesn’t exist.
-            </Typography>
+        <div>
+          <Typography variant="h1">404</Typography>
+          <Typography variant="h6">
+            The page you’re looking for doesn’t exist.
+          </Typography>
+          <Link to="/">
             <Button variant="contained">Back Home</Button>
-          </Grid>
-          <Grid xs={12} md={6}>
-            <img src={Logo} alt="" className="rotate" />
-          </Grid>
-        </Grid>
+          </Link>
+        </div>
+        <div>
+          <img src={Logo} alt="" className="rotate" />
+        </div>
       </Container>
     </Box>
   );
