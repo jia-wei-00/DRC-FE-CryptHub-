35122ee6e56f6ef5acab93e1608753d3003f7c7e
jia-wei-00 <wwwbingo6@gmail.com>
2023-07-16 16:05:55 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       rollupOptions: {
-        external: [/^node:.*/],
+        external: ["intro.js/introjs.css"],
       },
     },
   };
